90553b79c683b410aa218cae2f990fac0fa1f6ae
Raphael Arce <raphael.arce@hotmail.com>
2022-08-20 11:07:08 +0200

    added allow cors headers

--- hello-world/app.js
+++ hello-world/app.js
@@ -28,6 +28,11 @@ exports.lambdaHandler = async (event, context) => {
 
         response = {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,GET"
+            },
             body: {
                 products
             }
