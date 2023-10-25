b29e4bf4fd14d3573f3469de5208dbc36f3ca198
Raphael Arce <raphael.arce@ts.berlin>
2023-10-25 22:26:55 +0200

    chore: added missing test for app entry point

--- localize-serverless/app.js
+++ localize-serverless/app.js
@@ -1,42 +1,40 @@
-const searchEngine = require("./src/search/searchEngine");
+const searchEngine = require('./src/search/searchEngine');
 
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
-exports.lambdaHandler = async (event, context) => {
-    let response;
+exports.lambdaHandler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,GET',
+  };
 
-    try {
-        let q = '';
+  try {
+    let q = '';
 
-        if (event.queryStringParameters) {
-            q = event.queryStringParameters['q'] ?? '';
-        }
-
-        const result = JSON.stringify(await searchEngine.search(q));
-
-        response = {
-            statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Headers" : "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "OPTIONS,GET"
-            },
-            body: result
-        };
-    } catch (err) {
-        console.log(err);
-        return err;
+    if (event.queryStringParameters) {
+      q = event.queryStringParameters['q'] ?? '';
     }
 
-    return response
+    const result = JSON.stringify(await searchEngine.search(q));
+
+    return {
+      statusCode: 200,
+      headers,
+      body: result,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: 'Internal Server Error.',
+    };
+  }
 };
--- localize-serverless/app.spec.js
+++ localize-serverless/app.spec.js
@@ -1,15 +1,83 @@
+jest.mock('./src/search/searchEngine');
+const searchEngine = require('./src/search/searchEngine');
 const app = require('./app');
 
-describe('test', () => {
-    it('test', async () => {
-        const event = {
-            queryStringParameters: {
-                q: 'weleda tagescreme granatapfel'
-            }
-        }
+describe('app', () => {
+  it('should return a correctly formed response', async () => {
+    const givenEvent = {
+      queryStringParameters: {
+        q: 'weleda tagescreme granatapfel',
+      },
+    };
+    const givenSearchResults = [];
 
-        const result = await app.lambdaHandler(event, {});
+    jest
+      .spyOn(searchEngine, 'search')
+      .mockImplementationOnce(() => givenSearchResults);
 
-    })
+    const expectedResponse = {
+      body: '[]',
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: 200,
+    };
 
-})
+    const actualResponse = await app.lambdaHandler(givenEvent);
+
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  it('should return a correctly formed response when no query is given', async () => {
+    const givenEvent = {
+      queryStringParameters: {},
+    };
+    const givenSearchResults = [];
+
+    jest
+      .spyOn(searchEngine, 'search')
+      .mockImplementationOnce(() => givenSearchResults);
+
+    const expectedResponse = {
+      body: '[]',
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: 200,
+    };
+
+    const actualResponse = await app.lambdaHandler(givenEvent);
+
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+
+  it('should throw an error when search throws an error', async () => {
+    const givenEvent = {
+      queryStringParameters: {
+        q: 'weleda tagescreme granatapfel',
+      },
+    };
+
+    jest.spyOn(searchEngine, 'search').mockImplementationOnce(() => {
+      throw new Error('Something went wrong');
+    });
+
+    const expectedResponse = {
+      body: 'Internal Server Error.',
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET',
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: 500,
+    };
+
+    const actualResponse = await app.lambdaHandler(givenEvent);
+
+    expect(actualResponse).toEqual(expectedResponse);
+  });
+});
